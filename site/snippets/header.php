9a9ab0dab2656284732bb0ab2646e09c573f106c
Maxime <maxime@kaliel.fr>
2022-11-10 17:56:25 +0100

    defaults

---
+++ site/snippets/header.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @var \Kirby\Cms\App $kirby
+ * @var \Kirby\Cms\Site $site
+ */
+?>
+
+<!doctype html>
+<html lang="<?= $kirby->language()->code(); ?>">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Document</title>
+</head>
+<body>
