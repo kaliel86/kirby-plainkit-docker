53fc13237e9e554118de84cce513b1cd652d7333
Maxime <maxime@kaliel.fr>
2022-11-11 12:53:34 +0100

    tooling

--- Makefile
+++ Makefile
@@ -1,21 +1,52 @@
+user := $(shell id -u)
+group := $(shell id -g)
+dc := USER_ID=$(user) GROUP_ID=$(group) docker compose
+de := $(dc) exec
+dr := $(dc) run --rm
+node := $(dr) node
+php := $(dr) --no-deps php
+
 .PHONY: help
 help: ## Show this help
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
+.PHONY: dev
+dev: vendor/autoload.php node_modules/.yarn-integrity ## Run dev server
+	$(dc) up -d
+	@echo 'Docker up'
+	@echo 'PHP server is on : http://localhost:8096'
+	@echo 'Hot Reload server is on : http://localhost:3000'
+
 .PHONY: start
 start: ## Run docker container
-	docker compose up -d
+	$(dc) up -d
 	@echo 'Docker up. Server listening on : http://localhost:8096'
 
 .PHONY: stop
 stop: ## Stop docker container
-	docker compose down
+	$(dc) down
+	@echo 'Docker containers stopped'
 
 .PHONY: bash
 bash: ## bash into php container
-	docker compose exec php bash
+	$(de) php bash
 
 
 .PHONY: ci
 ci: ## Install dependencies in production environment
 	composer install --no-dev --ignore-platform-reqs
+
+## dependencies
+
+composer.lock: composer.json
+	$(php) composer update
+
+vendor/autoload.php: composer.lock
+	$(php) composer install
+	touch vendor/autoload.php
+
+yarn.lock: package.json
+	$(node) yarn
+
+node_modules/.yarn-integrity: yarn.lock
+	$(node) yarn
---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const {src, dest, watch, series} = require('gulp');
+const uglify = require('gulp-uglify');
+const {exec} = require('child_process');
+
+// css deps
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sass = require('gulp-sass')(require('node-sass'));
+const size = require('gulp-size');
+
+const browserSync = require('browser-sync').create();
+
+
+const sassFiles = 'dev/scss/**/*.scss';
+const jsFiles = 'dev/js/**/*.js';
+const phpFilesHotReload = 'site/**/*.php';
+const iconsFiles = 'dev/icons/*.svg';
+
+function js() {
+  return src(jsFiles)
+    .pipe(uglify())
+    .pipe(size())
+    .pipe(dest('./assets/js'));
+}
+
+function css() {
+  const plugins = [
+    autoprefixer(),
+    cssnano({
+      discardComments: {
+        removeAll: true
+      },
+      discardDuplicates: true,
+      discardEmpty: true,
+      minifyFontValues: true,
+      minifySelectors: true,
+      zindex: false
+    })
+  ];
+
+  return src(sassFiles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(browserSync.stream())
+    .pipe(postcss(plugins))
+    .pipe(size())
+    .pipe(dest('assets/css'));
+}
+
+function reload() {
+
+
+  browserSync.init({
+    open: false,
+    proxy: 'http://php',
+    files: phpFilesHotReload
+  });
+
+  watch(sassFiles, css);
+  watch(jsFiles).on('change', series(js, browserSync.reload));
+  watch(iconsFiles, sprite);
+}
+
+function sprite() {
+  return exec('node sprite.js');
+}
+
+exports.js = js;
+exports.css = css;
+exports.default = reload;
