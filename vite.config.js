38aa8c42969acf0bd1acd62762212194d24f212c
max <maxime@kaliel.fr>
2024-01-08 20:38:46 +0100

    vite allow multiples inputs

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import {defineConfig} from 'vite';
+import {glob} from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 
 export function CustomHmr() {
@@ -23,10 +26,24 @@ export function CustomHmr() {
 export default defineConfig({
   plugins: [CustomHmr()],
   build: {
+    outDir: 'assets/',
+    assetsDir: '',
     manifest: true,
     cssMinify: 'lightningcss',
     rollupOptions: {
-      input: 'dev/js/common.js',
+      input: Object.fromEntries(
+        glob.sync('dev/**/[^_]*.{js,scss}').map(file => [
+          // This remove `src/` as well as the file extension from each
+          // file, so e.g. src/nested/foo.js becomes nested/foo
+          path.relative(
+            'dev',
+            file.slice(0, file.length - path.extname(file).length)
+          ),
+          // This expands the relative paths to absolute paths, so e.g.
+          // src/nested/foo becomes /project/src/nested/foo.js
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
     },
   },
 })
